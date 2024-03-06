aefe0b8c1e452fce32551a2527ce064eef844d44
Netero03 <jatinletsgo@gmail.com>
2024-03-07 05:10:38 +0530

    commit

---
+++ client/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import icon from "../public/favicon.png";
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
+import { useAuthContext } from "../context";
+
+const Navbar = () => {
+  const { address, IsOrg } = useAuthContext();
+
+  return (
+    <div className={styles.container}>
+      <a href="/">SecureStamp</a>
+      <ul className={styles.links}>
+        <li>
+          <Link href="/verify">Verify</Link>
+        </li>
+        {address && IsOrg && (
+          <li>
+            <Link href="/certificate">Issue Certificate</Link>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ client/context/index.tsx
@@ -0,0 +1,71 @@
+import { SmartContract, useAddress, useContract } from "@thirdweb-dev/react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import {contractAbi} from '../constants';
+export interface Auth {
+  address: string | undefined;
+  Contract: SmartContract | undefined;
+  IsOrg: boolean;
+  loading: boolean | undefined;
+}
+
+export const AuthContext = createContext<Auth | undefined>(undefined);
+
+export const AuthStateProvider = ({ children }: { children: ReactNode }) => {
+  const { contract } = useContract(
+    "0x645e8D604c0b0E3DFbE869bEfB48BA2520f85eF3",contractAbi
+  );
+
+  const _address = useAddress();
+  const [IsOrganization, setIsOrganization] = useState(false);
+  const [Loading, setLoading] = useState<boolean>();
+
+  const [auth, setAuth] = useState<Auth>({
+    address: _address,
+    Contract: contract,
+    IsOrg: false,
+    loading: true,
+  });
+
+  useEffect(() => {
+    const getOrganization = async () => {
+      try {
+        if (contract && _address) {
+          setLoading(true);
+          console.log(contract);
+          const data = await contract.call("isOrganization", [_address]);
+          setIsOrganization(data);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (!IsOrganization) getOrganization();
+
+    setAuth({
+      address: _address,
+      Contract: contract,
+      IsOrg: IsOrganization,
+      loading: Loading,
+    });
+  }, [_address, IsOrganization, contract]);
+
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+};
+
+export const useAuthContext = () => {
+  const auth = useContext(AuthContext);
+
+  if (auth == undefined) {
+    throw new Error("Auth Object Exists..");
+  }
+
+  return auth;
+};
